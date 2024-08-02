3c29cf7f9e8cb279c6b3da765c1fe3706a0afc3e
Jenna Perkin <45342929+ssesses@users.noreply.github.com>
2024-08-02 11:13:02 -0400

    movement

--- Comp Games Demo/Assets/Scripts/CameraController.cs
+++ Comp Games Demo/Assets/Scripts/CameraController.cs
@@ -7,30 +7,35 @@ public class CameraController : MonoBehaviour
 public Transform target;
 public Vector3 offset;
 public float rotationSensitivity;
+public Transform pivot;
 
     // Start is called before the first frame update
     void Start()
     {
         offset = target.position - transform.position;
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = target.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Get X and Y mouse position and rotate target accordingly
         float horizontal = Input.GetAxis("Mouse X") * rotationSensitivity;
         target.Rotate(0, horizontal, 0);
 
         float vertical = Input.GetAxis("Mouse Y") * rotationSensitivity;
-        target.Rotate(vertical, 0, 0);
+        pivot.Rotate(vertical, 0, 0);
 
         //Move camera based on target rotation
         float YAngle = target.eulerAngles.y;
-        float XAngle = target.eulerAngles.x;
+        float XAngle = pivot.eulerAngles.x;
 
         Quaternion rotation = Quaternion.Euler(XAngle, YAngle, 0);
         transform.position = target.position - (rotation * offset);
 
-        //transform.position = target.position - offset;
         transform.LookAt(target);
     }
 }
--- Comp Games Demo/Assets/Scripts/PlayerController.cs
+++ Comp Games Demo/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,24 +9,61 @@ public class PlayerController : MonoBehaviour
     public float movementSpeed;
     public Rigidbody rb;
     public float jumpSpeed;
-    private float distanceToGround;
     public LayerMask FloorMask;
     public Transform GroundCheckTransform;
 
+    private Vector3 horizontalMove;
+    private Vector3 verticalMove;
 
+    // public GameObject Camera;
+    // private Vector2 move, look;
+    // private float lookRotation;
+    // public float maxForce;
+
+    // public void OnMove(InputAction.CallbackContext context)
+    // {
+    //     move = context.ReadValue<Vector2>();
+    // }
+
+    // public void OnLook(InputAction.CallbackContext context)
+    // {
+    //     look = context.ReadValue<Vector2>();
+    // }
+
+    // private void FixedUpdate()
+    // {
+    //     Vector3 currentVelocity = rb.velocity;
+    //     Vector3 targetVelocity = new Vector3(move.x, 0, move.y);
+    //     targetVelocity *= movementSpeed;
+
+    //     targetVelocity = transform.TransformDirection(targetVelocity);
+
+    //     Vector3 velocityChange = (targetVelocity - currentVelocity);
+
+    //     Vector3.ClampMagnitude(velocityChange, maxForce);
+    //     rb.AddForce(velocityChange, ForceMode.VelocityChange);
+
+    //     rb.AddForce(velocityChange,ForceMode.VelocityChange);
+    // }
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
 
-        distanceToGround = GetComponent<Collider>().bounds.extents.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector3(Input.GetAxis("Horizontal") * movementSpeed, rb.velocity.y, Input.GetAxis("Vertical") * movementSpeed);
+        horizontalMove = Input.GetAxis("Horizontal") * transform.right;
+        verticalMove = Input.GetAxis("Vertical") * new Vector3(transform.forward.x, 0f, transform.forward.z);
+
+        Vector3 direction = horizontalMove + verticalMove;
+
+        transform.position += direction * movementSpeed * Time.deltaTime;
+        //rb.velocity = new Vector3(Input.GetAxis("Horizontal") * movementSpeed, rb.velocity.y, Input.GetAxis("Vertical") * movementSpeed);
+
 
         if(Input.GetButtonDown("Jump"))
         {
